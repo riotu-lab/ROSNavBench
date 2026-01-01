b450f3e55b3c15f72b6a5775fbbef09d7e8c8d18
asmbatati <asmalbatati@hotmail.com>
2026-01-01 15:10:27 +0300

    Fixed turtlebot3 path

--- setup.py
+++ setup.py
@@ -5,11 +5,31 @@ from pathlib import Path
 
 package_name = 'ROSNavBench'
 
+
+def package_files(data_files, directory_list):
+    paths_dict = {}
+    for (path, directory) in directory_list:
+        for (path, directories, filenames) in os.walk(directory):
+            for filename in filenames:
+                file_path = os.path.join(path, filename)
+                install_path = os.path.join('share', package_name, directory, os.path.relpath(file_path, directory))
+                
+                # Check if the path is already in the dictionary
+                if install_path in paths_dict:
+                    paths_dict[install_path].append(file_path)
+                else:
+                    paths_dict[install_path] = [file_path]
+
+    for key in paths_dict.keys():
+        data_files.append((os.path.dirname(key), paths_dict[key]))
+
+    return data_files
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
+    data_files=package_files([
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
@@ -31,7 +51,10 @@ setup(
         (os.path.join('share/' + package_name, 'simulations/urdf/'), glob('simulations/urdf/*')),
         (os.path.join('share/' + package_name, 'example/turtlebot3/nav2_config'), glob('example/turtlebot3/nav2_config/*')),
         (os.path.join('share/' + package_name, 'example/turtlebot3/behavior_trees'), glob('example/turtlebot3/behavior_trees/*')),
-    ],
+
+    ], [
+        ('share/' + package_name + '/simulations/models/turtlebot3_description/', 'simulations/models/turtlebot3_description/'),
+    ]),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Fatimah-Alahmed',
